3dc609794005a0407d665772a20046451071f3fe
Fraga <fraga16@ua.pt>
2019-10-15 16:13:29 +0100

    details

--- ros_ws/src/cork_iris/src/load.cpp
+++ ros_ws/src/cork_iris/src/load.cpp
@@ -45,26 +45,57 @@ int main(int argc, char **argv){
     ImageParser ip;
     Mat loadedimg;
     fs["img"] >> loadedimg;
+    Mat gray;
+    cvtColor(loadedimg, gray, CV_BGR2GRAY);
+    Scalar m = mean(gray);
+    int media = (int)m[0];
+    printf("%d\n", media);
 
     // ip.extendDepthImageColors(loadedimg);
     // loadedimg = ip.thresholdImage(loadedimg, 50);
     std::vector<std::vector<cv::Point>> conts;
-    conts = ip.parseImageContours(loadedimg, 50);
-    conts = ip.filterContoursByArea(conts, 500, 10000);
-
-
+    // conts = ip.parseImageContours(loadedimg, 50);
+    // conts = ip.filterContoursByArea(conts, 500, 10000);
+    // Rect roi(95, 70 , 400, 380);
+    // loadedimg = loadedimg(roi);
 
     // draw those contours
-    drawImageContours(loadedimg, conts);
-    std::vector<cv::RotatedRect> rect_points;
-    rect_points = ip.getContourBoundingBox(conts);
-    drawContourBoundingBox(loadedimg, rect_points);
-
+    // drawImageContours(loadedimg, conts);
+    // std::vector<cv::RotatedRect> rect_points;
+    // rect_points = ip.getContourBoundingBox(conts);
+    // drawContourBoundingBox(loadedimg, rect_points);
+    
+    int i = 50;
     while(1){
+
+        conts = ip.parseImageContours(loadedimg, media);
+        conts = ip.filterContoursByArea(conts, 500, 8000);
+
+        // draw those contours
+        drawImageContours(loadedimg, conts);
+        std::vector<cv::RotatedRect> rect_points;
+        rect_points = ip.getContourBoundingBox(conts);
+        drawContourBoundingBox(loadedimg, rect_points);
         imshow("Display", loadedimg);
 
         // wait for quit key
-        if(waitKey(15) == 27 && 0xFF) break;
+        if(waitKey(0) == 27 && 0xFF) break;
+        else if(waitKey(0) == 99 && 0xFF) {
+
+         
+            conts.clear();
+            fs["img"] >> loadedimg;
+            // loadedimg = loadedimg(roi);
+               cv::putText(loadedimg, //target image
+            std::to_string(i), //text
+            cv::Point(10, loadedimg.rows / 2), //top-left position
+            cv::FONT_HERSHEY_DUPLEX,
+            1.0,
+            CV_RGB(118, 185, 0), //font color
+            2);
+            if(i < 250) i+=5;
+            else break;
+        } 
 
     }
 
