97b283e950cb39469a30e8029a7e7bf9a5ac54ef
Fábio Alves <fabioalves98@ua.pt>
2019-10-16 16:12:06 +0100

    image with pins with light

--- ros_ws/src/cork_iris/src/load.cpp
+++ ros_ws/src/cork_iris/src/load.cpp
@@ -57,60 +57,61 @@ int main(int argc, char **argv){
 
     // printf("Rows - %d; Cols - %d; Size - %d\n", loadedimg.rows, loadedimg.cols, loadedimg.total());
 
-    // std::vector<Point> possible_pins;
+    std::vector<Point> possible_pins;
 
-    // for (int i = 0; i < loadedimg.rows; i++)
-    // {
-    //     for (int j = 0; j < loadedimg.cols; j++)
-    //     {
-    //         Vec3b pixel = loadedimg.at<cv::Vec3b>(i,j);
-    //         int r = static_cast<int>(pixel.val[2]);
-    //         int g = static_cast<int>(pixel.val[1]);
-    //         int b = static_cast<int>(pixel.val[0]);
+    for (int i = 0; i < loadedimg.rows; i++)
+    {
+        for (int j = 0; j < loadedimg.cols; j++)
+        {
+            Vec3b pixel = loadedimg.at<cv::Vec3b>(i,j);
+            int r = static_cast<int>(pixel.val[2]);
+            int g = static_cast<int>(pixel.val[1]);
+            int b = static_cast<int>(pixel.val[0]);
 
-    //         if ((r > 120) && (g * 2.5 < r) && (b * 2.5 < r) && (abs(g - b) - 30)) 
-    //         {
-    //             possible_pins.push_back(Point(j, i));
-    //             //printf("Found, X: %d, Y: %d\n", j, i);
-    //             //circle(loadedimg, Point(j,i), 2, Scalar(250, 0, 0));
-    //             //printf("   R: %d, G: %d, B: %d\n", r, g, b);
-    //         }
-    //     }
-    // }
+            if ((r > 150) && (g * 3 < r) && (b * 3 < r) && (abs(g - b) - 30)) 
+            {
+                possible_pins.push_back(Point(j, i));
+                //printf("Found, X: %d, Y: %d\n", j, i);
+                //circle(loadedimg, Point(j,i), 2, Scalar(250, 0, 0));
+                //printf("   R: %d, G: %d, B: %d\n", r, g, b);
+            }
+        }
+    }
 
-    // // print(possible_pins);
-    // std::vector<Point> good_pins;
+    print(possible_pins);
+    
+    std::vector<Point> good_pins;
 
-    // for (int i = 0; i < possible_pins.size(); i++)
-    // {
-    //     Point candidate = possible_pins.at(i);
-    //     bool already_good = false;
-    //     for (int k = 0; k < good_pins.size(); k++)
-    //     {
-    //         if (norm(Mat(candidate), Mat(good_pins.at(k))) < 10 )
-    //         {
-    //             already_good = true;
-    //         }
-    //     }
-    //     if (already_good) continue;
+    for (int i = 0; i < possible_pins.size(); i++)
+    {
+        Point candidate = possible_pins.at(i);
+        bool already_good = false;
+        for (int k = 0; k < good_pins.size(); k++)
+        {
+            if (norm(Mat(candidate), Mat(good_pins.at(k))) < 10 )
+            {
+                already_good = true;
+            }
+        }
+        if (already_good) continue;
 
-    //     int count = 0;
-    //     for (int j = 0; j < possible_pins.size(); j++)
-    //     {
-    //         Point comp = possible_pins.at(j);
+        int count = 0;
+        for (int j = 0; j < possible_pins.size(); j++)
+        {
+            Point comp = possible_pins.at(j);
 
-    //         if (norm(Mat(candidate), Mat(comp)) < 10)
-    //         {
-    //             count ++;
-    //         }
-    //         if (count > 5)
-    //         {
-    //             good_pins.push_back(candidate);
-    //             circle(loadedimg, candidate, 5, Scalar(250, 0, 0));
-    //             break;
-    //         }
-    //     }
-    // }
+            if (norm(Mat(candidate), Mat(comp)) < 10)
+            {
+                count ++;
+            }
+            if (count > 5)
+            {
+                good_pins.push_back(candidate);
+                circle(loadedimg, candidate, 5, Scalar(250, 0, 0));
+                break;
+            }
+        }
+    }
 
     // print(good_pins);
     // printf("\n");
