ed634513113d92c8609b505ddb6dc29fe20285be
Fraga <fraga16@ua.pt>
2019-11-15 14:01:25 +0000

    added pcl visualizer, now shows pointcloud + normals ('debug' mode tho)

--- ros_ws/src/cork_iris/src/pcl_points.cpp
+++ ros_ws/src/cork_iris/src/pcl_points.cpp
@@ -5,15 +5,53 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
-#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/features/normal_3d.h>
+// #include <pcl/visualization/cloud_viewer.h>
 
+// pcl::visualization::CloudViewer viewer ("Simple Cloud Viewer");
 
-pcl::visualization::CloudViewer viewer ("Simple Cloud Viewer");
+boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer;
+
+// Temporary variable for debugging
+bool temp_one_time_compute = false;
 ros::Publisher pub;
+
+boost::shared_ptr<pcl::visualization::PCLVisualizer> normals_vis()
+{   
+    boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
+    viewer->setBackgroundColor (0, 0, 0);
+    viewer->addCoordinateSystem (1.0);
+    viewer->initCameraParameters ();
+    return (viewer);
+}
+
+void setViewerPointcloud(pcl::PointCloud<pcl::PointXYZRGBA>::ConstPtr cloud, 
+                         pcl::PointCloud<pcl::Normal>::ConstPtr cloud_normals)
+{
+    pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGBA> rgb(cloud);
+    viewer->addPointCloud<pcl::PointXYZRGBA> (cloud, rgb, "sample cloud");
+    viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "sample cloud");
+    viewer->addPointCloudNormals<pcl::PointXYZRGBA, pcl::Normal> (cloud, cloud_normals, 10, 0.05, "normals");
+
+}
+
+
+void viewerRunner(boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer)
+{
+    while (!viewer->wasStopped ())
+    {
+        viewer->spinOnce (100);
+        boost::this_thread::sleep (boost::posix_time::microseconds (100000));
+    }
+}
+
+
 void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 {
     pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGBA>);
+    pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
+    pcl::search::KdTree<pcl::PointXYZRGBA>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZRGBA> ());
 
     try{
         pcl::fromROSMsg (*cloud_msg, *cloud);
@@ -22,31 +60,26 @@ void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 		return;
 	}
 
-    std::cout << "Starting normals computation" << std::endl;
-    // Estimating Surface Normals
-    pcl::NormalEstimation<pcl::PointXYZRGBA, pcl::Normal> ne;
-    ne.setInputCloud (cloud);
+    if(!temp_one_time_compute)
+    {
+        std::cout << "Computing normals" << std::endl;
+        pcl::NormalEstimation<pcl::PointXYZRGBA, pcl::Normal> ne;
+        ne.setInputCloud (cloud);
 
-    pcl::search::KdTree<pcl::PointXYZRGBA>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZRGBA> ());
-    ne.setSearchMethod (tree);
+        ne.setSearchMethod (tree);
 
-    pcl::PointCloud<pcl::Normal>::Ptr cloud_normals (new pcl::PointCloud<pcl::Normal>);
 
-    ne.setRadiusSearch (0.03);
+        ne.setRadiusSearch (0.03);
+        ne.compute (*cloud_normals);
 
-    ne.compute (*cloud_normals);
-    std::cout << "Finalized normals computation" << std::endl;
+        setViewerPointcloud(cloud, cloud_normals);
+        temp_one_time_compute = true;
+        std::cout << "Finished computing normals" << std::endl;
 
-    // sensor_msgs::PointCloud2 msg;
-    // pcl::toROSMsg(*cloud_normals, msg);
-    // std::cout << msg.header.frame_id << std::endl;
-    // std::cout << msg.width<< std::endl;
-    // pub.publish(msg);
-    
-    if(!viewer.wasStopped()){
-        viewer.showCloud(cloud);
+        
     }
-    
+
+    viewerRunner(viewer);
 }
 
 int main (int argc, char** argv)
@@ -57,6 +90,7 @@ int main (int argc, char** argv)
     // Create a ROS subscriber for the input point cloud
     ros::Subscriber sub = nh.subscribe<sensor_msgs::PointCloud2> ("/camera/depth_registered/points", 1, cloud_cb);
 
+    viewer = normals_vis();
     // Create a ROS publisher for the output point cloud
     pub = nh.advertise<sensor_msgs::PointCloud2> ("/newtopics/processed_depth", 1);
     ros::Rate loop_rate(10);
