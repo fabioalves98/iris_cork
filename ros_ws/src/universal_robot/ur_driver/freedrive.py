fead332797149ee2a1f9866bf83745c51d0be298
Fábio Alves <fabiomcalves98@gmail.com>
2019-12-17 16:40:34 +0000

    ur packages added to workspace

---
+++ ros_ws/src/universal_robot/ur_driver/freedrive.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+
+def main():
+    rospy.init_node("test_freedrive", anonymous=True)
+    pub = rospy.Publisher('/ur_driver/URScript', String, queue_size = 1)
+    print("Waiting so connection is established");
+    rospy.sleep(5)
+
+    while not rospy.is_shutdown():
+        print("sending script ");
+        urs = String("def prog():\nfreedrive_mode()\nend\n")
+        pub.publish(urs);
+        rospy.sleep(1)
+
+if __name__ == '__main__': main()
+
