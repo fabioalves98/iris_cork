bca0ddac13ea78635263c5b1cf978a3702ec2b8d
Fraga <fraga16@ua.pt>
2020-10-14 15:54:34 +0100

    menu in arm_cmd_sender

--- ros_ws/src/cork_iris/src/arm_cmd_sender.py
+++ ros_ws/src/cork_iris/src/arm_cmd_sender.py
@@ -3,32 +3,49 @@ import rospy, socket, sys
 from cork_iris.msg import ArmCommand
 from cork_iris.srv import ControlArm
 
-def main():
 
-    rospy.init_node('arm_command_sender', anonymous=False)
-
-
-    # cmd_pub = rospy.Publisher('cork_iris/control', ArmCommand, queue_size=1)
-    # arguments = sys.argv[1:]
-    # data = ArmCommand()
-    # data.command = arguments
-    # rospy.loginfo(data.command)
-    # while not rospy.is_shutdown():
-    #     connections = cmd_pub.get_num_connections()
-    #     if connections > 0:
-    #         cmd_pub.publish(data)
-    #         rospy.loginfo('Command sent')
-            # break
-
-    arguments = sys.argv[1:]
+def callControlArmService(arguments, printFeedback=True):
     rospy.wait_for_service('/cork_iris/control_arm', timeout=2.5)
     try:
         control_arm = rospy.ServiceProxy('/cork_iris/control_arm', ControlArm)
         feedback = control_arm(arguments)
         rospy.loginfo("Command sent successfully") if feedback.success else rospy.logerr("Command returned with an error")
-        rospy.loginfo(feedback.output_feedback)
+        if printFeedback: rospy.loginfo(feedback.output_feedback)
+        return feedback.output_feedback
     except rospy.ServiceException as e:
         print("Service call failed: %s"%e)
+
+
+def main():
+
+    rospy.init_node('arm_command_sender', anonymous=False)
+
+    arguments = sys.argv[1:]
+    if arguments:
+        # rospy.wait_for_service('/cork_iris/control_arm', timeout=2.5)
+        # try:
+        #     control_arm = rospy.ServiceProxy('/cork_iris/control_arm', ControlArm)
+        #     feedback = control_arm(arguments)
+        #     rospy.loginfo("Command sent successfully") if feedback.success else rospy.logerr("Command returned with an error")
+        #     rospy.loginfo(feedback.output_feedback)
+        # except rospy.ServiceException as e:
+        #     print("Service call failed: %s"%e)
+        callControlArmService(arguments)
+    
+    helpStr = callControlArmService(["help"], printFeedback=False)
+    helpStr = helpStr[helpStr.find("\n"):]
+    while not rospy.is_shutdown():
+        rospy.loginfo(helpStr)
+        arguments = raw_input("Insert command: ").split(" ")
+        if("exit" in arguments):
+            break
+        callControlArmService(arguments)
+        rospy.sleep(1.0)
+    
+    # rospy.spin()
                 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
