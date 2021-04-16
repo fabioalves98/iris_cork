259f8d68effaada307d55c89eb89dde3a345985d
Fraga <fraga16@ua.pt>
2021-04-16 18:31:20 +0100

    Fixed some bugs and improved shape segmentation

--- pcl_tests/Test4/test.cpp
+++ pcl_tests/Test4/test.cpp
@@ -18,6 +18,7 @@
 #include <pcl/filters/extract_indices.h>
 #include <unistd.h>
 #include <pcl/io/point_cloud_image_extractors.h>
+#include <pcl/segmentation/conditional_euclidean_clustering.h>
 // #include <pcl/conversions.h>
 
 pcl::visualization::PCLVisualizer::Ptr viewer;
@@ -44,6 +45,31 @@ void setViewerPointcloud(pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr cloud)
     pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb(cloud);
     viewer->addPointCloud<pcl::PointXYZRGB> (cloud, rgb, "kinectcloud");
     viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "kinectcloud");
+}
+
+void  smoothCloud(pcl::PointCloud<PointXYZRGB>::Ptr in, pcl::PointCloud<PointXYZRGB>::Ptr out){
+    // Create a KD-Tree
+    pcl::search::KdTree<PointXYZRGB>::Ptr tree (new pcl::search::KdTree<PointXYZRGB>);
+
+    // Output has the PointNormal type in order to store the normals calculated by MLS
+    pcl::PointCloud<pcl::PointNormal> mls_points;
+
+    // Init object (second point type is for the normals, even if unused)
+    pcl::MovingLeastSquares<PointXYZRGB, pcl::PointNormal> mls;
+
+    mls.setComputeNormals (true);
+
+    // Set parameters
+    mls.setInputCloud (in);
+    mls.setPolynomialOrder (3);
+    mls.setSearchMethod (tree);
+    mls.setSearchRadius (0.03);
+
+    // Reconstruct
+    mls.process (mls_points);
+    copyPointCloud(mls_points, *out);
+
+
 }
 
 
@@ -89,205 +115,187 @@ BoundingBox computeCloudBoundingBox(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud
 
     return bb;
 }
+void shapeBasedSegmentation(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_in, pcl::IndicesClustersPtr clusters_out)
+{
+
+    pcl::KdTreeFLANN<pcl::PointXYZRGB> kdtree;
+    kdtree.setInputCloud (cloud_in);
+    pcl::PointXYZRGB searchPoint;
+
+    int K = 45;
+    std::vector<int> adjacent_idx(K);
+    std::vector<float> adjacent_distance(K);
+    std::set<int> searched;
+    
+
+    std::vector<double> last_dir = {0.0,0.0,0.0};
+    std::vector<int> currently_growing = {-1, -1, -1};// Keeps track of last three iterations.
+    int grow_track = 2; // 2 porque estamos a procura do comprimento. isto pode ser um problema? talvez mas parece que o comprimento esta sempre ok
+    int cluster_idx = 0;
+    
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr subcloud (new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::PointIndicesPtr subcloud_indices (new pcl::PointIndices);
+
+  
+    
+    // pcl::PointCloud<pcl::PointXYZRGB>::Ptr subcloud2 (new pcl::PointCloud<pcl::PointXYZRGB>);
+
+    for(int idx = 0; idx < cloud_in->points.size(); idx++){
+        
+        // if(!std::count(searched.begin(), searched.end(), idx)){
+        if(searched.find(idx) == searched.end()){
+            searchPoint = cloud_in->at(idx);
+            // std::cout << "Search point(" << idx << "): " << searchPoint << std::endl;
+        }else{
+            // std::cout << "Point search ignored(" << idx << ")" << std::endl;
+            continue;
+        }
+        adjacent_idx.clear();
+        adjacent_distance.clear();
+
+        if (kdtree.nearestKSearch(searchPoint, K, adjacent_idx, adjacent_distance) > 0){
+            for (std::size_t i = 0; i < adjacent_idx.size (); ++i){
+                // std::cout << "POINT OF SEARCH: " << pointIdxNKNSearch[i] << std::endl;
+                // std::cout << "    "  <<   (*cloud_in)[ pointIdxNKNSearch[i] ].x 
+                //             << " " << (*cloud_in)[ pointIdxNKNSearch[i] ].y 
+                //             << " " << (*cloud_in)[ pointIdxNKNSearch[i] ].z 
+                //             << " (squared distance: " << pointNKNSquaredDistance[i] << ")" << std::endl;
+                if(cluster_idx == 0){
+                    (*cloud_in)[adjacent_idx[i]].r = 255;
+                    (*cloud_in)[adjacent_idx[i]].g = 255;
+                    (*cloud_in)[adjacent_idx[i]].b = 0;
+                }else if(cluster_idx == 1){
+                (*cloud_in)[adjacent_idx[i]].r = 255;
+                (*cloud_in)[adjacent_idx[i]].g = 0;
+                (*cloud_in)[adjacent_idx[i]].b = 255;    
+                }else{
+                (*cloud_in)[adjacent_idx[i]].r = 0;
+                    (*cloud_in)[adjacent_idx[i]].g = 255;
+                    (*cloud_in)[adjacent_idx[i]].b = 255;   
+                }
+                subcloud->points.push_back((*cloud_in)[adjacent_idx[i]]);
+                subcloud_indices->indices.push_back(adjacent_idx[i]);
+                // }
+                searched.insert(adjacent_idx[i]);
+            }
+        }
+
+  
+
+        BoundingBox bb = computeCloudBoundingBox(subcloud);
+        double compr = (bb.maxPoint.z - bb.minPoint.z) ;
+        double largura = (bb.maxPoint.y - bb.minPoint.y) ;
+        double altura = (bb.maxPoint.x - bb.minPoint.x) ;
+        std::vector<double> dims = {abs(largura - last_dir[1]), abs(compr- last_dir[2])}; // ignoramos a altura. talvez possa ser um problema, mas parece que altura esta sempre certa
+        int growing_side = (std::max_element(dims.begin(), dims.end()) - dims.begin()) + 1;
+
+        std::cout << "Currently tracking side: " << grow_track << std::endl;
+        std::cout << "Growing side: " << growing_side << std::endl;
+        currently_growing.erase(currently_growing.begin());            
+        currently_growing.push_back(growing_side);
+        std::cout << "growing queue: " << currently_growing[0] << " " << currently_growing[1] << " " << currently_growing[2] << std::endl;
+        std::cout << "dims : " << dims[0] << " " << dims[1]      << std::endl;
+        
+        // if(grow_track == -1){
+        //     if(currently_growing[2] == currently_growing[1]){
+        //         grow_track = growing_side;
+        //     }
+        // }
+
+        // if((currently_growing[2] != grow_track && currently_growing[1] != grow_track && currently_growing[0] != grow_track && currently_growing[1] != -1) || (searched.size() == cloud_in->size())){
+        if((currently_growing[2] != grow_track && currently_growing[1] != grow_track && currently_growing[0] != -1) || (searched.size() == cloud_in->size())){
+            // std::cout << "COUNT: "<<  std::count(currently_growing.begin(), currently_growing.end(), grow_track) << std::endl;
+            std::cout << " CHANGED DIRECTION! STOP CLUSTER!" << std::endl;
+            std::cout << " searched == cloudin " <<  (searched.size() == cloud_in->size()) << std::endl;
+            std::cout << " dims0 > dims1 " <<  (dims[0] > 10*dims[1]) << std::endl;
+            // grow_track = -1;
+            currently_growing[0] = currently_growing[1] = currently_growing[2] = -1;
+            // pcl::PointIndices indices (new pcl::PointIndices);
+            // indices.indices = subcloud_indices.indices;
+            clusters_out->push_back(*subcloud_indices);
+            // std::cout << *subcloud_indices << std::endl;
+            subcloud_indices->indices.clear();
+            // std::cout<< "AFTER CLEAR:" << *subcloud_indices << std::endl;
+            // std::cout<< "AFTER CLEAR:" << clusters_out[0] << std::endl;
+            subcloud->points.clear();
+            // grow_track = -1;
+
+            cluster_idx++;
+        
+        }    
+        last_dir.at(0) = altura;
+        last_dir.at(1) = largura;
+        last_dir.at(2) = compr;
+        std::cout << "-------" << std::endl;
+        usleep(100000);
+
+        viewer->removeShape("cork_piece");
+        viewer->addCube(bb.position, bb.orientation, bb.maxPoint.x - bb.minPoint.x, bb.maxPoint.y - bb.minPoint.y, bb.maxPoint.z - bb.minPoint.z, "cork_piece", 0);  
+        viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR, 1, 0, 0, "cork_piece");             
+        viewer->setRepresentationToWireframeForAllActors(); 
+        viewer->updatePointCloud(cloud_in, "kinectcloud");
+        viewer->spinOnce();
+
+    }
+
+
+}
 
 int main (int argc, char** argv)
 {
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_in (new pcl::PointCloud<pcl::PointXYZRGB>);
     char temp[100];
     std::string ye = getcwd(temp, sizeof(temp)) ? std::string( temp ) : std::string("");
 
     // pcl::io::savePCDFile("./big_segment.pcd", *(clusters[i].cloud), true);
     std::cout << "current  dir: " << ye << std::endl;
 
-    if (pcl::io::loadPCDFile<pcl::PointXYZRGB> ("t_shape.pcd", *cloud) == -1) //* load the file
+    if (pcl::io::loadPCDFile<pcl::PointXYZRGB> ("big_segment.pcd", *cloud_in) == -1) //* load the file
     {
         PCL_ERROR ("Couldn't read file big_segmented.pcd \n");
             std::cout << "eheh: " << std::endl;
 
         return (-1);
     }
-    std::cout << "Loaded "
-        << cloud->width * cloud->height
-        << " data points from big_segmented.pcd with the following fields: "
-        << std::endl;
+    // std::cout << "Loaded "
+    //     << cloud->width * cloud->height
+    //     << " data points from big_segmented.pcd with the following fields: "
+    //     << std::endl;
 
 
     
 
     viewer = normalVis("3DViewer");
-    setViewerPointcloud(cloud);
+    setViewerPointcloud(cloud_in);
+
+    smoothCloud(cloud_in, cloud_in);
+    pcl::IndicesClustersPtr out_indices (new pcl::IndicesClusters);
+    shapeBasedSegmentation(cloud_in, out_indices);
+
+    for(int i = 0; i < out_indices->size(); i++){
 
 
-
-    pcl::KdTreeFLANN<pcl::PointXYZRGB> kdtree;
-
-    kdtree.setInputCloud (cloud);
-
-    pcl::PointXYZRGB searchPoint;
-
-    int K = 30;
-    std::vector<int> adjacent_idx(K);
-    std::vector<float> adjacent_distance(K);
-
-    std::vector<int> searched(cloud->size());
-    // searched.push_back(0);
-    std::vector<int> to_search(cloud->size());
-    std::iota(to_search.begin(), to_search.end(), 0);
-    // for(int i = 0; i < to_search.size(); i++){
-    //     std::cout << to_search[i] << std::endl; 
-
-    // }
-    // std::cin;
-    to_search.erase(std::remove(to_search.begin(), to_search.end(), 0), to_search.end());
-
-
-    // std::vector<int> current_dir = {0,0,0};
-    
-
-    std::vector<double> last_dir = {0.0,0.0,0.0};
-    std::vector<int> currently_growing = {-1, -1, -1};// Keeps track of last three iterations.
-    int grow_track = 2; // 2 porque estamos a procura do comprimento. isto pode ser um problema? talvez mas parece que o comprimento esta sempre ok
-
-
-    // pcl::PointCloud<pcl::PointXYZRGB>::Ptr subcloud (new pcl::PointCloud<pcl::PointXYZRGB>);
-
-    int cluster_idx = 0;
-
-    while(!viewer->wasStopped())
-    {
-        
-        pcl::PointCloud<pcl::PointXYZRGB>::Ptr subcloud (new pcl::PointCloud<pcl::PointXYZRGB>);
-        pcl::PointCloud<pcl::PointXYZRGB>::Ptr subcloud2 (new pcl::PointCloud<pcl::PointXYZRGB>);
-
-        for(int idx = 0; idx < cloud->points.size(); idx++){
-            if(!std::count(searched.begin(), searched.end(), idx)){
-                searchPoint = cloud->at(idx);
-                std::cout << "Search point(" << idx << "): " << searchPoint << std::endl;
+        for (int j = 0; j < (*out_indices)[i].indices.size (); ++j){
+            if(i == 0){
+                (*cloud_in)[(*out_indices)[i].indices[j]].r = 255;
+                (*cloud_in)[(*out_indices)[i].indices[j]].g = 255;
+                (*cloud_in)[(*out_indices)[i].indices[j]].b = 0;  
+            }else if(i == 1){
+                (*cloud_in)[(*out_indices)[i].indices[j]].r = 255;
+                (*cloud_in)[(*out_indices)[i].indices[j]].g = 0;
+                (*cloud_in)[(*out_indices)[i].indices[j]].b = 255;    
             }else{
-                // std::cout << "Point search ignored(" << idx << ")" << std::endl;
-                continue;
+               (*cloud_in)[(*out_indices)[i].indices[j]].r = 0;
+                (*cloud_in)[(*out_indices)[i].indices[j]].g = 255;
+                (*cloud_in)[(*out_indices)[i].indices[j]].b = 255;   
             }
-            adjacent_idx.clear();
-            adjacent_distance.clear();
-
-            if (kdtree.nearestKSearch(searchPoint, K, adjacent_idx, adjacent_distance) > 0){
-                
-                
-                for (std::size_t i = 0; i < adjacent_idx.size (); ++i){
-                    // std::cout << "POINT OF SEARCH: " << pointIdxNKNSearch[i] << std::endl;
-                    // std::cout << "    "  <<   (*cloud)[ pointIdxNKNSearch[i] ].x 
-                    //             << " " << (*cloud)[ pointIdxNKNSearch[i] ].y 
-                    //             << " " << (*cloud)[ pointIdxNKNSearch[i] ].z 
-                    //             << " (squared distance: " << pointNKNSquaredDistance[i] << ")" << std::endl;
-                    if(cluster_idx == 0){
-                        (*cloud)[adjacent_idx[i]].r = 255;
-                        (*cloud)[adjacent_idx[i]].g = 255;
-                        (*cloud)[adjacent_idx[i]].b = 0;
-                        subcloud->points.push_back((*cloud)[ adjacent_idx[i]]);
-                    }else{
-                        (*cloud)[adjacent_idx[i]].r = 0;
-                        (*cloud)[adjacent_idx[i]].g = 255;
-                        (*cloud)[adjacent_idx[i]].b = 255;    
-                        subcloud2->points.push_back((*cloud)[ adjacent_idx[i]]);
-                    }
-                    searched.push_back(adjacent_idx[i]);
-                    to_search.erase(std::remove(to_search.begin(), to_search.end(), adjacent_idx[i]), to_search.end());
-                    // }else{
-                    //     (*cloud)[adjacent_idx[i]].r = 0;
-                    //     (*cloud)[adjacent_idx[i]].g = 255;
-                    //     (*cloud)[adjacent_idx[i]].b = 255;    
-                    //     subcloud2->points.push_back((*cloud)[ adjacent_idx[i]]);
-
-                    // }
-
-                }
-
         }
+    }
 
-            BoundingBox bb;
-            if(cluster_idx == 0) bb = computeCloudBoundingBox(subcloud);
-            else bb = computeCloudBoundingBox(subcloud2);
-            double compr = (bb.maxPoint.z - bb.minPoint.z) ;
-            double largura = (bb.maxPoint.y - bb.minPoint.y) ;
-            double altura = (bb.maxPoint.x - bb.minPoint.x) ;
-            std::vector<double> dims = {largura - last_dir[1], compr- last_dir[2]}; // ignoramos a altura. talvez possa ser um problema, mas parece que altura esta sempre certa
-            int growing_side = (std::max_element(dims.begin(), dims.end()) - dims.begin()) + 1;
+    viewer->updatePointCloud(cloud_in, "kinectcloud");
+    while(!viewer->wasStopped())
+        viewer->spinOnce();
 
-            std::cout << "Currently tracking side: " << grow_track << std::endl;
-            std::cout << "Growing side: " << growing_side << std::endl;
-            currently_growing.erase(currently_growing.begin());            
-            currently_growing.push_back(growing_side);
-            std::cout << "growing queue: " << currently_growing[0] << " " << currently_growing[1] << " " << currently_growing[2] << std::endl;
-            
-            // Just started
-            // if(grow_track == -1){
-            //     // if(std::equal(currently_growing.begin() + 1, currently_growing.end(), currently_growing.begin())){
-            //         grow_track = currently_growing[currently_growing.size() - 1];
-            
-                // }
-            // }else{
-                // if(std::equal(currently_growing.begin() + 1, currently_growing.end(), currently_growing.begin()) && currently_growing[0] != grow_track){
-        
-                if(currently_growing[2] != grow_track && currently_growing[1] != grow_track && currently_growing[1] != -1){
-                    std::cout << "COUNT: "<<  std::count(currently_growing.begin(), currently_growing.end(), grow_track) << std::endl;
-                // if(currently_growing[2] != grow_track && currently_growing[1] != grow_track){
-                    std::cout << " CHANGED DIRECTION! STOP CLUSTER!" << std::endl;
-                    // grow_track = -1;
-                    currently_growing[0] = currently_growing[1] = currently_growing[2] = -1;
-                    cluster_idx++;
-                    if(cluster_idx == 2) break;
-                
-                }    
-            // }
-
-            std::cout << "last dir 2: " << last_dir[2] << std::endl;
-            std::cout << "last dir 1: " << last_dir[1] << std::endl;
-            std::cout << "last dir 0: " << last_dir[0] << std::endl;
-
-            std::cout << "diff comprimento: " << dims[2]<< std::endl;
-            std::cout << "diff largura: " << dims[1] << std::endl;
-            std::cout << "diff altura: " << dims[2] << std::endl;
-
-            if(growing_side == 2){
-                std::cout << "comprimento A AUMENTAR" << std::endl;
-            }
-            if(growing_side == 1){
-                std::cout << "largura A AUMENTAR" << std::endl;
-            }
-            if(growing_side == 0){
-                std::cout << "altura A AUMENTAR" << std::endl;
-            }
-
-            last_dir.at(0) = altura;
-            last_dir.at(1) = largura;
-            last_dir.at(2) = compr;
-
-            viewer->removeShape("bb");
-            viewer->addCube(bb.position, bb.orientation, bb.maxPoint.x - bb.minPoint.x, bb.maxPoint.y - bb.minPoint.y, bb.maxPoint.z - bb.minPoint.z, "bb", 0);  
-            viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR, 1, 0, 0, "bb");             
-            viewer->setRepresentationToWireframeForAllActors(); 
-
-            viewer->updatePointCloud(cloud, "kinectcloud");
-
-            usleep(500000);
-
-            viewer->spinOnce();
-            std::cout << "------: "<< std::endl;
-
-            // std::cout << idx << std::endl;
-            // searchPoint = cloud->at(idx);
-            // std::cout << searchPoint << std::endl;
-
-        }
-
-        while(!viewer->wasStopped())
-            viewer->spinOnce();
-// 
-        // setViewerPointcloud(subcloud);
-
-        // PointCloud<PointXYZ>::Ptr cloud_xyz;
-        // copyPointCloud(*cloud_xyz, *cloud);
-            }
-    // std::cout << "hiii" << std::endl;
+   
     return (0);
 }
